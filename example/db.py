04d2b0b6651e0e9b4a40dbc3131272b0d212cb96
tekinbomer <tekinbomer@gmail.com>
2025-06-04 19:49:37 +0300

    Create db.py

---
+++ example/db.py
@@ -0,0 +1,10 @@
+import mysql.connector
+import os
+
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME")
+    )
