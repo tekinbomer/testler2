6cdd8f6624aad1ed1d66e0aa8135c77b65ad9fac
tekinbomer <tekinbomer@gmail.com>
2025-06-05 13:37:03 +0300

    Update vapi_key.py

--- example/vapi_key.py
+++ example/vapi_key.py
@@ -1,12 +1,11 @@
 from vapid import Vapid01
 
-# mailto: eposta (zorunlu)
-email = "mailto:siparis@tarotalemi.com"
-
-# Anahtarları oluştur
-vapid = Vapid01()
-private_key, public_key = vapid.create_keys()
-vapid.set_claims(sub=email)
-
-print("🔐 VAPID Public Key:\n", public_key.decode())
-print("\n🗝️ VAPID Private Key:\n", private_key.decode())
+def generate_keys():
+    email = "mailto:siparis@tarotalemi.com"
+    vapid = Vapid01()
+    private_key, public_key = vapid.create_keys()
+    vapid.set_claims(sub=email)
+    return {
+        "public": public_key.decode(),
+        "private": private_key.decode()
+    }
